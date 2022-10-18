e25963b9181f449f8722bf71f22e218d1d796e8c
Ksenia <ksenechka.93@gmail.com>
2022-10-18 10:51:12 +0300

    feat: made module for markup generation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 import {generateObjects, OBJECTS_TO_GENERATE} from './generate-objects.js';
+import {generateMarkup, popupMap} from './popup.js';
 
-generateObjects(OBJECTS_TO_GENERATE);
+const obj = generateObjects(OBJECTS_TO_GENERATE);
+const markup = generateMarkup(obj[0]);
+popupMap.appendChild(markup);
