39491be7da51b0e4cd9edd7698110452571fce63
Ksenia <ksenechka.93@gmail.com>
2022-11-01 10:46:50 +0300

    fix: change syntax to arrow functions

--- js/validation.js
+++ js/validation.js
@@ -3,6 +3,8 @@ const roomNumberElement = adForm.querySelector('#room_number');
 const capacityElement = adForm.querySelector('#capacity');
 const priceElement = adForm.querySelector('#price');
 const typeElement = adForm.querySelector('#type');
+const checkinElement = adForm.querySelector('#timein');
+const checkoutElement = adForm.querySelector('#timeout');
 
 const roomsToGuests = {
   1: ['1'],
@@ -26,27 +28,48 @@ const typesToMinPrices = {
   palace: 10000,
   max: 100000,
 };
+
 const pristine = new Pristine(adForm, {
   classTo: 'ad-form__element',
   errorClass: 'ad-form__element--invalid',
   errorTextParent: 'ad-form__element',
 }, true);
+
 //функции валидации и сообщений об ошибках
-function validateCapacity () {
-  return roomsToGuests[roomNumberElement.value].includes(capacityElement.value);
-}
-function getCapacityErrorMessage () {
-  return `Указанные комнаты могут вместить ${roomsToGuests[roomNumberElement.value].join(' или ')} гостя(ей)`;
-}
-function getRoomNumberErrorMessage () {
-  return `Для выбранного количества гостей требуется ${guestsToRooms[capacityElement.value].join(' или ')} комнат(ы)`;
-}
-function validatePrice(value) {
-  return (value >= typesToMinPrices[typeElement.value]) && (value <= typesToMinPrices.max);
-}
-function getPriceErrorMessage() {
-  return `Минимальная стоимость для выбранного типа жильа ${typesToMinPrices[typeElement.value]} руб.`;
-}
+const validateCapacity = () => (
+  roomsToGuests[roomNumberElement.value].includes(capacityElement.value)
+);
+const getCapacityErrorMessage = () => (
+  `Указанные комнаты могут вместить ${roomsToGuests[roomNumberElement.value].join(' или ')} гостя(ей)`
+);
+const getRoomNumberErrorMessage = () => (
+  `Для выбранного количества гостей требуется ${guestsToRooms[capacityElement.value].join(' или ')} комнат(ы)`
+);
+const validatePrice = (value) => (
+  (value >= typesToMinPrices[typeElement.value]) && (value <= typesToMinPrices.max)
+);
+const getPriceErrorMessage = () =>(
+  `Минимальная стоимость для выбранного типа жильа ${typesToMinPrices[typeElement.value]} руб.`
+);
+
+//автоматическое изменение полей времени заезда/выезда
+const onCheckinChange = () => {
+  checkinElement.value = checkoutElement.value;
+};
+const onCheckoutChange = () => {
+  checkoutElement.value = checkinElement.value;
+};
+//изменеие цены в зависимости от выбранного типа жилья
+const onTypeChange = () => {
+  const minPrice = typesToMinPrices[typeElement.value];
+  priceElement.placeholder = minPrice;
+  priceElement.min = minPrice;
+  pristine.validate(priceElement);
+};
+//проверка формы при изменеии ее полей
+const onFormChange = () => {
+  pristine.validate();
+};
 
 pristine.addValidator(
   capacityElement,
@@ -63,6 +86,11 @@ pristine.addValidator(
   validatePrice,
   getPriceErrorMessage
 );
+
+adForm.addEventListener('change', onFormChange);
+checkinElement.addEventListener('change', onCheckoutChange);
+checkoutElement.addEventListener('change', onCheckinChange);
+typeElement.addEventListener('change', onTypeChange);
 
 adForm.addEventListener('submit', (evt) => {
   if (!pristine.validate()) {
