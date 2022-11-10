29cd36a693af41efd7967c46a9e118266f1c0603
Keks <keks@htmlacademy.ru>
2022-11-10 13:43:14 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,8 @@
+import { initSlider } from './slider.js';
+import {setMap, renderOffersOnMap} from './map.js';
+import { setOfferFormSubmit} from './form-validation.js';
+
+initSlider();
+setMap();
+renderOffersOnMap();
+setOfferFormSubmit();
