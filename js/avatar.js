50d29c9f2f20094eb7a026206127cbb607a08640
Ksenia <ksenechka.93@gmail.com>
2022-11-11 10:36:47 +0300

    feat: file choosing option

---
+++ js/avatar.js
@@ -0,0 +1,41 @@
+const TYPES_OF_FILE = ['jpg', 'jpeg', 'png'];
+
+const fileChooserAvatar = document.querySelector('#avatar');
+const previewAvatar = document.querySelector('.ad-form-header__preview img');
+const fileChooserPlace = document.querySelector('#images');
+const previewPlace = document.querySelector('.ad-form__photo');
+const container = document.querySelector('.ad-form__photo-container');
+
+const checkType = (fileName) =>
+  TYPES_OF_FILE.some((it) => fileName.endsWith(it));
+
+
+fileChooserAvatar.addEventListener('change', () => {
+  const file = fileChooserAvatar.files[0];
+  if(checkType(file.name.toLowerCase())) {
+    previewAvatar.src = URL.createObjectURL(file);
+  }
+
+});
+
+fileChooserPlace.addEventListener('change', () => {
+  const files = fileChooserPlace.files;
+  const fragment = document.createDocumentFragment();
+  for (const file of files) {
+    if (checkType(file.name.toLowerCase())) {
+      previewPlace.remove();
+      const div = document.createElement('div');
+      div.classList.add('ad-form__photo');
+      const img = document.createElement('img');
+      img.src = URL.createObjectURL(file);
+      img.alt = 'Фото жилья';
+      img.style.width = '100%';
+      img.style.height = '100%';
+      img.style.objectFit = 'cover';
+      div.appendChild(img);
+      fragment.appendChild(div);
+    }
+  }
+  container.appendChild(fragment);
+});
+
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { initSlider } from './slider.js';
 import {setMap, renderOffersOnMap} from './map.js';
 import { setOfferFormSubmit} from './form-validation.js';
+import './avatar.js';
 
 initSlider();
 setMap();
